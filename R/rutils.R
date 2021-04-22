0ebefe9e77b527bffe64f3558b462fe2995a1500
rfmanz <rafael.arevalo27@gmail.com>
2021-04-22 15:21:38 -0500

    intial

--- R/rutils.R
+++ R/rutils.R
@@ -5,4 +5,3 @@ library(data.table)
 pct_nulls = function(df)
   df[,.(names = names(df), count_nulls = lapply(.SD, function(x) sum(is.na(x))), pct= lapply(.SD, function(x) scales::percent(sum(is.na(x))/nrow(df),0.01)))][order(as.numeric(count_nulls)),]
 
-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
