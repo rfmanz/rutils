6f6ae78bb6a01ed590a4b66081cdebabbf3d917a
rfmanz <rafael.arevalo27@gmail.com>
2021-04-22 17:37:07 -0500

    rmlist

--- R/rutils.R
+++ R/rutils.R
@@ -1,4 +1,8 @@
+#' @export
 # Find nas by column----
 pct_nulls = function(df)
   df[,.(names = names(df), count_nulls = lapply(.SD, function(x) sum(is.na(x))), pct= lapply(.SD, function(x) scales::percent(sum(is.na(x))/nrow(df),0.01)))][order(as.numeric(count_nulls)),]
-
+#' @export
+# remove all variables
+rmlist = function()
+  rm(list=ls())
