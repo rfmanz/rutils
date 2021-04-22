cc8c8069ff8a0ed3f83143d223c7ff544197940d
rfmanz <rafael.arevalo27@gmail.com>
2021-04-22 16:16:07 -0500

    a versh

--- R/rutils.R
+++ R/rutils.R
@@ -1,7 +1,5 @@
-library(rutils)
-#train  = fread(cmd = 'unzip -p /home/r/Downloads/tabular-playground-series-apr-2021.zip  train.csv')
-
 # Find nas by column----
 pct_nulls = function(df)
   df[,.(names = names(df), count_nulls = lapply(.SD, function(x) sum(is.na(x))), pct= lapply(.SD, function(x) scales::percent(sum(is.na(x))/nrow(df),0.01)))][order(as.numeric(count_nulls)),]
+
 
