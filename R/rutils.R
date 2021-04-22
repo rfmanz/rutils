f91f27de738a82553360b91ab91905e19ad65870
rfmanz <rafael.arevalo27@gmail.com>
2021-04-22 17:30:31 -0500

    namepsace

--- R/rutils.R
+++ R/rutils.R
@@ -2,4 +2,3 @@
 pct_nulls = function(df)
   df[,.(names = names(df), count_nulls = lapply(.SD, function(x) sum(is.na(x))), pct= lapply(.SD, function(x) scales::percent(sum(is.na(x))/nrow(df),0.01)))][order(as.numeric(count_nulls)),]
 
-
