e8e23b017de229cbfe985a17988832bb7ba8628b
rfmanz <rafael.arevalo27@gmail.com>
2021-04-23 19:31:45 -0500

    d

--- churn_dataset.R
+++ churn_dataset.R
@@ -70,7 +70,7 @@ train[,.(Names = names(.SD),
          Pct_Nulls = lapply(.SD, function(x) percent(sum(is.na(x))/nrow(.SD),0.01)),
          Count_Nulls = lapply(.SD, function(x) sum(is.na(x))),
          Count_Not_Null = lapply(.SD, function(x) sum(!is.na(x))),
-         #Levels = lapply(.SD, function(x) length(levels(as.factor(x)))),
+         Levels = lapply(.SD, function(x) length(levels(as.factor(x)))),
          Unique_values = fifelse(lapply(.SD, function(x) length(levels(as.factor(x)))) > 5, lapply(.SD, function(x) length(levels(as.factor(x)))), lapply(.SD, function(x)levels(as.factor(x))))),
       .SDcols = is.character]
 
@@ -79,42 +79,50 @@ train[,.(Names = names(.SD),
 describe_df(r.train)
 cat(names(table(as.factor(train$PaymentMethod))),sep = "|")
 
-
+knitr::kable(train)
 
 prop.table(table(is.na(train$TotalCharges)))*100
 
 describe_df = function(df){
-print("Numeric Variables")
-df[,.(Names = names(.SD),
-        Min = lapply(.SD,function(x) min(x,na.rm=TRUE)),
-        Mean = lapply(.SD,function(x)mean(x,na.rm=TRUE)),
-        Mode= lapply(.SD,fmode),
-        pct_Nulls = lapply(.SD, sum(is.na))),
-     .SDcols = is.numeric]}
+   numeric = df[,.(Names = names(.SD),
+           Min = lapply(.SD,function(x) round(min(x,na.rm=TRUE),0)),
+           Mean = lapply(.SD,function(x) round(mean(x,na.rm=TRUE),0)),
+           Max = lapply(.SD,function(x) round(max(x,na.rm=TRUE),0)),
+           Mode= lapply(.SD, function(x) round(fmode(x),0)),
+           Pct_Nulls = lapply(.SD, function(x) percent(sum(is.na(x))/nrow(.SD),0.01)),
+           Count_Nulls = lapply(.SD, function(x) sum(is.na(x))),
+           Count_Not_Null = lapply(.SD, function(x) sum(!is.na(x)))),
+        .SDcols = is.numeric]
+
+  categorical = df[,.(Names = names(.SD),
+           Pct_Nulls = lapply(.SD, function(x) percent(sum(is.na(x))/nrow(.SD),0.01)),
+           Count_Nulls = lapply(.SD, function(x) sum(is.na(x))),
+           Count_Not_Null = lapply(.SD, function(x) sum(!is.na(x))),
+           Levels = lapply(.SD, function(x) length(levels(as.factor(x)))),
+           Unique_values = fifelse(lapply(.SD, function(x) length(levels(as.factor(x)))) > 5, lapply(.SD, function(x) length(levels(as.factor(x)))), lapply(.SD, function(x)levels(as.factor(x))))),
+        .SDcols = is.character]
+
+#output = list(numeric, categorical)
+comment(numeric)= '-----------------------------------NUMERIC--------------------------------------'
+comment(categorical)= '------------------------------CATEGORICAL---------------------------------------'
+for (i in list(numeric, categorical)) {
+  print_empty_line()
+  print(comment(i))
+      print(i)
+
+    print_empty_line()
+}
+}
 
 describe_df(train)
 
-Mode(train$tenure)
-describe(train$tenure)
-table(cut2(train$tenure,cuts = c(1,4,8,9)))
 
-mfv(train$StreamingMovies, na_rm = TRUE)
-str(train)
+deparse(substitute(train))
+print("Numeric Variables")
 
-for (j in which(sapply(${1:object},class)=='character')) {
-
-  set(${1:object},j=j, value = toupper(trimws(iconv(${1:object}[[j]],from = "UTF-8", to = "LATIN1"))))
-
-  set(${1:object}, i = which(is.na(${1:object}[[j]])|is.null(${1:object}[[j]])|${1:object}[[j]]==" "|${1:object}[[j]]=="NULL"|${1:object}[[j]]=="NA"|${1:object}[[j]]==""), j = j, value = NA_char
-
-      acter_)
-
-}
+print("Categorical Variables")
 
 
-plot_bar(train$Churn)
-
-str(train_nonulls2encoded)
 
 
 regression = glm(train_nonulls2encoded$Churn~.,train_nonulls2encoded[,-"Churn"], family = binomial)
