038fe33aef9cef7fa1418dd1f3063514c8075602
rfmanz <rafael.arevalo27@gmail.com>
2021-04-23 20:35:42 -0500

    s

--- R/rutils.R
+++ R/rutils.R
@@ -6,3 +6,35 @@ pct_nulls = function(df)
 # remove all variables
 rmlist = function()
   rm(list=ls())
+#' @export
+describe_df = function(df){
+  numeric = df[,.(Names = names(.SD),
+                  Min = lapply(.SD,function(x) round(min(x,na.rm=TRUE),0)),
+                  Mean = lapply(.SD,function(x) round(mean(x,na.rm=TRUE),0)),
+                  Max = lapply(.SD,function(x) round(max(x,na.rm=TRUE),0)),
+                  Mode= lapply(.SD, function(x) round(fmode(x),0)),
+                  Pct_Nulls = lapply(.SD, function(x) percent(sum(is.na(x))/nrow(.SD),0.01)),
+                  Count_Nulls = lapply(.SD, function(x) sum(is.na(x))),
+                  Count_Not_Null = lapply(.SD, function(x) sum(!is.na(x)))),
+               .SDcols = is.numeric]
+
+  categorical = df[,.(Names = names(.SD),
+                      Pct_Nulls = lapply(.SD, function(x) percent(sum(is.na(x))/nrow(.SD),0.01)),
+                      Count_Nulls = lapply(.SD, function(x) sum(is.na(x))),
+                      Count_Not_Null = lapply(.SD, function(x) sum(!is.na(x))),
+                      Levels = lapply(.SD, function(x) length(levels(as.factor(x)))),
+                      Unique_values = fifelse(lapply(.SD, function(x) length(levels(as.factor(x)))) > 5, lapply(.SD, function(x) length(levels(as.factor(x)))), lapply(.SD, function(x)levels(as.factor(x))))),
+                   .SDcols = is.character]
+
+  #output = list(numeric, categorical)
+  comment(numeric)= '-----------------------------------NUMERIC--------------------------------------'
+  comment(categorical)= '------------------------------CATEGORICAL---------------------------------------'
+  for (i in list(numeric, categorical)) {
+    print_empty_line()
+    print(comment(i))
+    print(i)
+
+    print_empty_line()
+  }
+}
+
